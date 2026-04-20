97b166bc5b25658fc050f8386b6055cb67c4b205
lgrmatheo-lgtm <lagiermatheo@gmail.com>
2026-04-20 11:39:16 +0200

    Update ProjetModel.js

--- projet/models/ProjetModel.js
+++ projet/models/ProjetModel.js
@@ -1,5 +1,4 @@
 // Models Projets
-
 const db = require("../../db");
 
 // Récupérer tous les projets
@@ -26,4 +25,17 @@ const searchProjets = async (search, limit) => {
     return rows;
 };
 
-module.exports = { getAllProjets, getProjetById, searchProjets };
+// Créer un nouveau projet
+const createProjet = async ({ title, description }) => {
+    const [result] = await db.query(
+        "INSERT INTO projet (titre, description) VALUES (?, ?)",
+        [title, description || ""]
+    );
+    const [rows] = await db.query(
+        "SELECT * FROM projet WHERE Id_projet = ?",
+        [result.insertId]
+    );
+    return rows[0];
+};
+
+module.exports = { getAllProjets, getProjetById, searchProjets, createProjet };
