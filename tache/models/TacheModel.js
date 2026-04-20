ae1687ad554730cb6d50f5216e5cce7924e2c472
samue <samuelawodun5@gmail.com>
2026-04-20 15:03:26 +0200

    ajout de fonctionnalités pour création et modification des tâches

---
+++ tache/models/TacheModel.js
@@ -0,0 +1,87 @@
+// Model Tâches
+
+const db = require("../../db");
+
+// Création d'une tâche
+const createTask = async (taskData) => {
+    // Extraction des données
+    const {
+        nom,
+        description,
+        statut,
+        date_echeance,
+        temps_prevu,
+        temps_reel
+    } = taskData;
+
+    const [result] = await db.query(
+        `INSERT INTO taches
+        (nom_tache, description, statut, date_echeance, temps_prevu, temps_reel)
+        VALUES (?, ?, ?, ?, ?, ?)`,
+        [
+           nom,
+           description,
+           statut,
+           date_echeance,
+           temps_prevu,
+           temps_reel
+        ],
+    );
+    return result;
+};
+
+// Récupérer une tâche par son ID
+const getTaskById = async (Id_tache) => {
+    const [rows] = await db.query(`SELECT * FROM taches WHERE Id_tache = ?`,
+        [Id_tache]
+    );
+    return rows;
+}
+
+// Modifier une tâche
+const updateTask = async (Id_tache, taskData) => {
+
+    // Récupération de tâche actuelle
+    const task = await getTaskById(Id_tache);
+
+    // Si tâche n'existe pas
+    if (task.length === 0) return null;
+
+    const actualTask = task[0];
+
+    // Si l'utilisateur n'envoie pas un champ, on garde l'ancienne valeur
+    const nom = taskData.nom ?? actualTask.nom_tache;
+    const description = taskData.description ?? actualTask.description;
+    const statut = taskData.statut ?? actualTask.statut;
+    const date_echeance = taskData.date_echeance ?? actualTask.date_echeance;
+    const temps_prevu = taskData.temps_prevu ?? actualTask.temps_prevu;
+    const temps_reel = taskData.temps_reel ?? actualTask.temps_reel;
+
+    const [result] = await db.query(
+        `UPDATE taches SET
+            nom_tache = ?,
+            description = ?,
+            statut = ?,
+            date_echeance = ?,
+            temps_prevu = ?,
+            temps_reel = ?
+        WHERE Id_tache = ?`,
+        [
+            nom,
+            description,
+            statut,
+            date_echeance,
+            temps_prevu,
+            temps_reel,
+            Id_tache,
+        ]
+    );
+
+    // Si aucune ligne modifiée, la tâche n'existe pas
+    if (result.affectedRows === 0) {
+        return null;
+    }
+    return result;
+};
+
+// Supprimer tâche
